b1f41715b0a4ba0cd5bf6c0b1ef74e3aa5884614
cjchallis <christopher.challis@gmail.com>
2016-11-11 13:23:07 +0000

    Django initialization

---
+++ functional_tests.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_load_review_and_delete_pictures(self):
+        # Hank heard about a new site to manage your pictures and goes to check it out
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention picture review
+        self.assertIn('Review Pictures', self.browser.title)
+        self.fail('Finish the test!')
+
+        # There is a list of folders to click on
+
+        # When he clicks a folder, a picture appears
+
+        # With the picture, there are options to keep, delete, or pass
+
+        # He passes on the picture, a new picture appears
+
+        # He deletes the next picture, a new picture appears
+
+        # He keeps the last picture
+
+        # The first picture reappears - he keeps this one as well
+
+        # A message appears that all pictures in this folder have been reviewed, and
+        # he is taken to the home page
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
