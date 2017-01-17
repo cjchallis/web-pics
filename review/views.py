5656e23301337111c783d0936be7d056c2ae29ca
cjchallis <christopher.challis@gmail.com>
2017-01-17 16:21:50 +0000

    Basic view now returns minimal HTML

--- review/views.py
+++ review/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>Review Pictures</title></html>')
 
