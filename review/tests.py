fc57e5d6f56135a239293c79788a5222c8ecaff6
cjchallis <christopher.challis@gmail.com>
2017-02-04 19:10:09 +0000

    Unit tests look for templates instead of html

--- review/tests.py
+++ review/tests.py
@@ -5,42 +5,20 @@ from django.template.loader import render_to_string
 
 from review.views import view_dir 
 
+
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_view_dir(self):
-        found = resolve('/')
-        self.assertEqual(found.func, view_dir)
+    def test_home_uses_dir_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = view_dir(request, "", "")
-        self.assertIn('dir1', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'entries': ['<a href="dir0/">dir0/</a>',
-                         '<a href="dir1/">dir1/</a>'],
-            }
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_dir_uses_dir_template(self):
+        response = self.client.get('/review/')
+        self.assertTemplateUsed(response, 'home.html')
 
 
-    def test_dir_url_resolves_to_view_dir(self):
-        found = resolve('/dir0/')
-        self.assertEqual(found.func, view_dir)
-
-
-    def test_dir_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = view_dir(request, "", "dir0/")
-        expected_html = render_to_string(
-            'home.html',
-            {'entries': ['<a href="dir00/">dir00/</a>',
-                         '<a href="dir01/">dir01/</a>',
-                         '<a href="pic0.jpg">pic0.jpg</a>',
-                         '<a href="pic1.jpg">pic1.jpg</a>',
-                        ],
-            }
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_file_uses_img_template(self):
+        response = self.client.get('/review/pic0.jpg')
+        self.assertTemplateUsed(response, 'img.html')
 
