b9c0791479f953a9c8db54ee8635433d34d713ac
cjchallis <christopher.challis@gmail.com>
2017-02-12 04:19:08 +0000

    Can move to next and previous images

--- review/views.py
+++ review/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os
 PIC_ROOT = os.path.join("/", "home", "pi", "tdd", "web_pics", "review",
                         "static") 
-
 
 def view_dir(request, empty, path):
     if path is None:
         path = ''
     contents = os.listdir(os.path.join(PIC_ROOT, path))
-    print(os.path.join(PIC_ROOT, path))
     dirs = [c + '/' for c in contents
             if os.path.isdir(os.path.join(PIC_ROOT, path, c))]
     dirs.sort()
@@ -23,8 +21,36 @@ def view_dir(request, empty, path):
     return render(request, 'home.html', args)
    
 
-def view_img(request, path):
-    img = os.path.split(path)[1]
-    return render(request, 'img.html', {'path': path,
+def view_img(request, nodepath):
+    img = os.path.split(nodepath)[1]
+    return render(request, 'img.html', {'path': nodepath,
                                         'img':  img})
+
+
+def nxt(request, nodepath):
+    path, node = os.path.split(nodepath)
+    contents = os.listdir(os.path.join(PIC_ROOT, path))
+    dirs = [c + '/' for c in contents
+            if os.path.isdir(os.path.join(PIC_ROOT, path, c))]
+    dirs.sort()
+    pics = [c for c in contents
+            if os.path.isfile(os.path.join(PIC_ROOT, path, c))]
+    pics.sort()
+    current = pics.index(node)
+    nxt = pics[(current + 1) % len(pics)]
+    return redirect("/{0}/{1}".format(path, nxt))
+
+
+def prev(request, nodepath):
+    path, node = os.path.split(nodepath)
+    contents = os.listdir(os.path.join(PIC_ROOT, path))
+    dirs = [c + '/' for c in contents
+            if os.path.isdir(os.path.join(PIC_ROOT, path, c))]
+    dirs.sort()
+    pics = [c for c in contents
+            if os.path.isfile(os.path.join(PIC_ROOT, path, c))]
+    pics.sort()
+    current = pics.index(node)
+    nxt = pics[(current - 1) % len(pics)]
+    return redirect("/{0}/{1}".format(path, nxt))
 
