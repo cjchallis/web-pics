4325bf5a16508fe046e6badf2f61c7a9a8a60fad
cjchallis <christopher.challis@gmail.com>
2017-03-09 03:25:28 +0000

    Add test case for deletion list

--- review/tests.py
+++ review/tests.py
@@ -7,21 +7,51 @@ from review.models import PicFile
 from review.views import view_dir 
 
 
-class HomePageTest(TestCase):
+class PageTest(TestCase):
 
-    def test_home_uses_dir_template(self):
+    def test_home_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
 
     def test_dir_uses_dir_template(self):
         response = self.client.get('/review/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'dir.html')
 
 
     def test_file_uses_img_template(self):
         response = self.client.get('/review/pic0.jpg')
         self.assertTemplateUsed(response, 'img.html')
+
+
+    def test_del_list_uses_del_template(self):
+        response = self.client.get('/deletion_list')
+        self.assertTemplateUsed(response, 'del_list.html')
+
+
+class DeletionTest(TestCase):
+
+    def test_delete_list(self):
+        first_file = PicFile()
+        first_file.path = 'review/pic0.png'
+        first_file.status = "delete"
+        first_file.save()
+
+        sec_file = PicFile()
+        sec_file.path = 'review/pic1.png'
+        sec_file.status = "delete"
+        sec_file.save()
+
+        thr_file = PicFile()
+        thr_file.path = 'review/pic2.png'
+        thr_file.path = 'Saved'
+        thr_file.save()
+
+        response = self.client.get('/deletion_list')
+        self.assertContains(response, 'review/pic0.png')
+        self.assertContains(response, 'review/pic1.png')
+        self.assertNotContains(response, 'review/pic2.png')
+
 
 class StatusModelTest(TestCase):
 
