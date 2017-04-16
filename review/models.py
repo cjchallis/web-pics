fcf96dc275180a51e13bd23d99116d28f69a6ec1
cjchallis <christopher.challis@gmail.com>
2017-04-16 15:19:11 +0000

    Change status of PicFile to choice field

--- review/models.py
+++ review/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 class PicFile(models.Model):
+    UNREVIEWED = 'UN'
+    KEEP = 'KP'
+    CHATBOOKS = 'CH'
+    DELETE = 'DL'
+    STATUS_CHOICES = (
+        (UNREVIEWED, 'Unreviewed'),
+        (KEEP, 'Keep'),
+        (CHATBOOKS, 'Junior'),
+        (SENIOR, 'Senior'),
+    )
     path = models.TextField(default='')
-    status = models.TextField(default='')
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default=UNREVIEWED,
+    )
 
