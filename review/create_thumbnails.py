9ecf79041aafa89d4521ce4b3b0fd6ced5221bed
cjchallis <christopher.challis@gmail.com>
2017-12-04 21:41:26 -0700

    Add script to generate pic thumbnails

---
+++ review/create_thumbnails.py
@@ -0,0 +1,21 @@
+import os
+
+cur_path = os.path.split(os.path.realpath(__file__))[0]
+pic_root = os.path.join(cur_path, "static", "review", "pics")
+thumb_root = os.path.join(cur_path, "static", "review", "thumbnails")
+
+
+pic_ext = [".png", ".jpg", ".bmp", ".gif"]
+
+for subdir, dirs, files in os.walk(pic_root):
+    sub = os.path.split(subdir)[1]
+    for f in files:
+        if os.path.splitext(f)[1].lower() in pic_ext:
+            thumb = os.path.join(thumb_root, sub, f)
+            pic = os.path.join(pic_root, sub, f)
+            if not os.path.exists(thumb):
+                d = os.path.join(thumb_root, sub)
+                if not os.path.exists(d):
+                    os.mkdir(d)
+                os.system("convert -thumbnail 300 '{0}' '{1}'".format(pic, thumb))
+
